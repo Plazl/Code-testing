e9f034b8725514f83103929c268001852f3deffa
jhnbg rth <I1234itsplaz@outlook.com>
2021-09-15 18:06:56 -0400

    Update test.js

--- test.js
+++ test.js
@@ -6,14 +6,15 @@ while (exist = true) {
 
 var clicks = clicks + 1;
      }
+if (clicks >= 1e31)  {
 
 const americanNames = [
   'k', 'm', 'b', 't', 'qa', 'qu', 'sx', 'sp', 'o', 'n',
  
 ];
 
-function formatBigPositiveNumber(n) {
-  const str = BigInt(n).toString();
+function formatBigPositiveNumber(clicks) {
+  const str = BigInt(clicks).toString();
   const len = str.length;
   if (len <= 3) {
     return str;
@@ -29,12 +30,29 @@ function formatBigPositiveNumber(n) {
   }
 }
 
-function fixedWidth(str, n) {
-  return (' '.repeat(n) + str).slice(-n);
+function fixedWidth(str, clicks) {
+  return (' '.repeat(clicks) + str).slice(-clicks);
 }
 
 for (let [input, expected] of clicks) {
   const actual = formatBigPositiveNumber(input);
   console.log('\n%s\n%s', fixedWidth(input, 25), fixedWidth(expected, 25));
   console.log('%s', fixedWidth(actual, 25));
+ }
+}  
+
+else if(clicks > 1e31) {
+     var formattedclicks = clicks.toExponential();
+  document.getElementById("clicks").innerHTML = formattedclicks;
+     
 }
+
+
+else {
+
+throw new Error("Something went wrong! Reload the page for a possible fix. If it wont fix, go to github.com/Plazl/Evo-Clicker and submut");
+  }
+     
+     
+     
+     
